7505e10a36b426b3f360fadc2de5d00ce7fd1898
antoinevandapel <vandapel.antoine@gmail.com>
2018-03-26 17:11:34 +0200

    ajout script

--- Exercice5/js/script.js
+++ Exercice5/js/script.js
@@ -1,8 +1,19 @@
-function hamburger_click()
-{
-  var hamburgerElement = document.getElementById("hamburger");
-  var menuElement = document.getElementById("menu");
- 
-  hamburgerElement.style.display = "none";
-  menuElement.style.display = "block";
-}
+$( document ).ready(function() {
+
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".hamburger" ).hide();
+            $( ".cross" ).show();
+            });
+        });
+
+    $( ".cross" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".cross" ).hide();
+            $( ".hamburger" ).show();
+        });
+    });
+
+});
